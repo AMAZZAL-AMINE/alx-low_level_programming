e9b5f452bd42d7b16c99b5410652f800ffcae39e
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2023-06-15 13:51:40 -0700

    init

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+* Description: C program to print "Programming is like building a multilingual puzzle" using the puts function.
+* Author: Mohamed Amine Amazzal
+* Date: 15/6/2023
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
