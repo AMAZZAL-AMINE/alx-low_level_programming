a78e9d0998efc61e2eb3a3ad0ebd3d057281dd74
Mohamed Amine Amazzal <mamazzal@c2r4p10.1337.ma>
2023-06-23 11:22:09 +0100

    init

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,51 +6,6 @@
  * Return: void
  */
 
-void print_times_table(int n)
-{
-	int i, j, k, l, m;
-
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-			for (j = 1; j <= n; j++)
-			{
-				k = i * j;
-				l = k / 100;
-				m = k % 100;
-				if (k < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(m + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(' ');
-					_putchar(l + '0');
-					_putchar(m + '0');
-				}
-				else
-				{
-					_putchar(l + '0');
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
-				}
-				if (j < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			_putchar('\n');
-		}
-	}
-}
-
 int main(void)
 {
 	print_times_table(3);
