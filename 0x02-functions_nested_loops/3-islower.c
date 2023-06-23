a361ae0314139f0d9c28a0fe1891a9e7472d9345
Mohamed Amine Amazzal <mamazzal@c2r4p10.1337.ma>
2023-06-23 10:52:27 +0100

    init

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
 /**
  * _islower - 3-islower.c
  * Return: 1 if lower case, 0 if not
-*/
+ */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-    return (1);
-  else
-    return (0);
+		return (1);
+	else
+		return (0);
 }
