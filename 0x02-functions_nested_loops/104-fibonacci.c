cfb0fa2f9b73cffc668a77a20d20b0a10156923d
Mohamed Amine Amazzal <mamazzal@c2r4p10.1337.ma>
2023-06-23 11:32:08 +0100

    gg

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,41 @@
 
 int main(void)
 {
-	unsigned long count, i, j, k;
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, result;
 
+	count = 0;
 	i = 0;
 	j = 1;
-	count = 0;
-	while (count < 50)
+	for (count = 1; count <= 91; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
-		printf("%lu", k);
-		if (count == 49)
-			putchar('\n');
+		printf("%lu, ", k);
+	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		result = (m + n) / 1000;
+		p = (m + n) - result * 1000;
+		k = (i + j) + result;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
 		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
 			printf(", ");
 		count++;
 	}
+	putchar('\n');
 	return (0);
 }
