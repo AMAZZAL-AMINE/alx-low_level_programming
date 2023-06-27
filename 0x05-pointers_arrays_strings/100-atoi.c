59b39fb3581755acec74e68f71481c4e4899c9e7
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2023-06-27 21:06:33 +0100

    init

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,23 +9,23 @@
 
 int	_atoi(char *s)
 {
-    int	i;
-    int	sign;
-    int	n;
+	int	i;
+	int	sign;
+	int	n;
 
-    i = 0;
-    sign = 1;
-    n = 0;
-    while ((*(s + i) < '0' || *(s + i) > '9') && *(s + i))
-    {
-        if (*(s + i) == '-')
-            sign *= -1;
-        i++;
-    }
-    while (*(s + i) >= '0' && *(s + i) <= '9')
-    {
-        n = n * 10 + sign * (*(s + i) - '0');
-        i++;
-    }
-    return (n);
+	i = 0;
+	sign = 1;
+	n = 0;
+	while ((*(s + i) < '0' || *(s + i) > '9') && *(s + i))
+	{
+		if (*(s + i) == '-')
+			sign *= -1;
+		i++;
+	}
+	while (*(s + i) >= '0' && *(s + i) <= '9')
+	{
+		n = n * 10 + sign * (*(s + i) - '0');
+		i++;
+	}
+	return (n);
 }
