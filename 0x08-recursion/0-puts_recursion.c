78ccfcc4522a7cd7a8fcea9682133b1fa530eda0
Mohamed Amine Amazzal <mamazzal@c2r4p10.1337.ma>
2023-07-06 19:47:24 +0100

    iniiiit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,13 @@
  * Return: void
  */
 
-void _puts_recursion(char *s)
+void	_puts_recursion(char *s)
 {
 	if (*s == '\0')
-		{
-			_putchar('\n');
-			return;
-		}
+	{
+		_putchar('\n');
+		return;
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
