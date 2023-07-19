1f82e598c02d844bf7f4a91d6cc33227f92877f4
Mohamed Amine Amazzal <mamazzal@c2r1p5.1337.ma>
2023-07-19 21:56:08 +0100

    init all

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,27 +11,27 @@
 
 int main(int argc, char *argv[])
 {
-  int i, bytes;
-  char *main_ptr;
+	int i, bytes;
+	char *main_ptr;
 
-  if (argc != 2)
-  {
-    printf("Error\n");
-    exit(1);
-  }
-  bytes = atoi(argv[1]);
-  if (bytes < 0)
-  {
-    printf("Error\n");
-    exit(2);
-  }
-  main_ptr = (char *)main;
-  for (i = 0; i < bytes; i++)
-  {
-    printf("%02hhx", main_ptr[i]);
-    if (i < bytes - 1)
-      printf(" ");
-  }
-  printf("\n");
-  return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	main_ptr = (char *)main;
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx", main_ptr[i]);
+		if (i < bytes - 1)
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
