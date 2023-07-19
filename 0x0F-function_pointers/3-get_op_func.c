7bda23860f22fac8a7551bf2c2fe6b7bb632b4ed
Mohamed Amine Amazzal <mamazzal@c2r1p5.1337.ma>
2023-07-19 21:51:31 +0100

    init all

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,19 +5,27 @@
  * @s: operator passed as argument
  *
  * Return: pointer to the function that corresponds to the operator given as a
- * parameter, or NULL if s does not match any of the 5 expected operators
+ * parameter, NULL if no such function exists
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add}, {"-", op_sub}, {"*", op_mul},
-		{"/", op_div}, {"%", op_mod}, {NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i;
 
-	for (i = 0; ops[i].op; i++)
-		if (*s == *ops[i].op)
+	i = 0;
+	while (ops[i].op)
+	{
+		if (s[0] == ops[i].op[0])
 			return (ops[i].f);
+		i++;
+	}
 	return (NULL);
 }
